d0608066cba5ef4ef10fa5e45b33c095d703d7a2
Nadir <nadirsalehsaleh@gmail.com>
2025-06-18 17:46:44 +0300

    COLLISION. THE PAIN.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
 #include <SFML/Graphics.hpp>
 #include <Windows.h>
+#include <vector>
 #include "resources.hpp"
 #include "animator.hpp"
 
 using namespace sf;
 
-const float maxSpeed=15, acceleration = 0.5, deceleration = 1.f, jumpVelocity = 26.f;
+const float maxSpeed=15, acceleration = 0.5, deceleration = 1.f, jumpVelocity = 10.f;
 bool isOnGround = 0;
 const float g = 0.4;
 
+
 Clock animClock, deltaClock;
+
+struct Wall
+{
+    Vector2f position, size;
+    RectangleShape hitbox;
+    Wall(Vector2f position, Vector2f size)
+    {
+        this->position = position;
+        this->size = size;
+        hitbox.setSize(size);
+        hitbox.setPosition(position);
+    }
+};
 
 struct playerObj
 {
@@ -17,6 +32,56 @@ struct playerObj
     Vector2f position;
     Vector2f velocity;
 };
+
+struct Level
+{
+    std::vector<Wall> walls;
+    Vector2f defaultPlayerPos = { 0, 0 };
+    Level() {}
+    Level(const std::vector<Wall>& ws) : walls(ws) {}
+};
+
+Level levels[5];
+int levelIndex = 0;
+
+bool isWallDown(playerObj player)
+{
+    FloatRect playerBounds = player.hitbox.getGlobalBounds();
+
+    player.hitbox.setPosition({player.position.x, player.position.y - player.velocity.y });
+    for (auto wall : levels[levelIndex].walls)
+    {
+        FloatRect wallBounds = wall.hitbox.getGlobalBounds();
+
+        if (playerBounds.findIntersection(wallBounds))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isWallUp(playerObj player)
+{
+    FloatRect playerBounds = player.hitbox.getGlobalBounds();
+
+    for (auto wall : levels[levelIndex].walls)
+    {
+        FloatRect wallBounds = wall.hitbox.getGlobalBounds();
+        if (playerBounds.findIntersection(wallBounds))
+        {
+            float playerTop = playerBounds.position.y;
+            float wallBottom = wallBounds.position.y + wallBounds.size.y;
+
+            if (playerTop <= wallBottom + 5.f && playerTop >= wallBottom - 5.f)
+                return true;
+        }
+    }
+    return false;
+}
+
+float dt;
+
 
 bool pressRight()
 {
@@ -45,19 +110,27 @@ bool pressJump()
     return false;
 }
 
-void updatePlayerPos(playerObj &player, const float dt)
+void updatePlayerPos(playerObj &player)
 {
 
-    if (player.position.y < 400)
+    isOnGround = isWallDown(player);
+
+
+    Vector2f lastPos = player.position;
+
+    if (player.position.y < 400 && !isOnGround)
         player.velocity.y += g * dt * (1 + !pressJump());
     else
-        player.position.y = 400, player.velocity.y = 0.f, isOnGround = true;
+        player.velocity.y = 0.f;
 
-    if (pressJump() && isOnGround)
+    if (pressJump() && (isOnGround || player.position.y>=400))
     {
-        player.velocity.y -= jumpVelocity * dt;
+        player.velocity.y -= jumpVelocity;
         isOnGround = false;
     }
+
+    if (isWallUp(player))
+        player.velocity.y = std::max(player.velocity.y, 0.f);
 
     if (pressRight())
     {
@@ -86,9 +159,30 @@ void updatePlayerPos(playerObj &player, const float dt)
                 player.velocity.x = 0;
         }
     }
-    player.position += player.velocity*dt;
 
-    player.hitbox.setPosition(player.position);
+    player.position.x += player.velocity.x*dt;
+    player.position.y += player.velocity.y * dt;
+
+    for (auto wall : levels[levelIndex].walls)
+    {
+        player.hitbox.setPosition(player.position);
+        if (!isOnGround)
+            if (player.hitbox.getGlobalBounds().findIntersection(wall.hitbox.getGlobalBounds()))
+            {
+                player.position = { lastPos.x, player.position.y };
+                player.hitbox.setPosition(player.position);
+                player.velocity.x = 0;
+            }
+    }
+}
+
+void loadLevels()
+{
+    levels[0].walls =
+    {
+        Wall({250, 250}, {50, 50}),
+        Wall({350, 350}, {500, 500})
+    };
 }
 
 int main()
@@ -97,12 +191,17 @@ int main()
     window.setFramerateLimit(144);
 
     playerObj player;
-    player.hitbox.setSize({ 50, 50 });
+    player.hitbox.setSize({ 32, 32 });
+
+    RectangleShape ground({720, 50});
+    ground.setPosition({ 0, 440 });
+
+    loadLevels();
 
     while (window.isOpen())
     {
 
-        float dt = deltaClock.restart().asSeconds() * 60; //60fps is considered the base
+        dt = deltaClock.restart().asSeconds() * 60; //60fps is considered the base
 
         while (const std::optional event = window.pollEvent())
         {
@@ -112,10 +211,13 @@ int main()
             }
         }
 
-        updatePlayerPos(player, dt);
+        updatePlayerPos(player);
 
         window.clear();
         window.draw(player.hitbox);
+        window.draw(ground);
+        for (auto wall : levels[levelIndex].walls)
+            window.draw(wall.hitbox);
         window.display();
     }
 }
